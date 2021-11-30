c18aad80bcec95f15d6616f1344c1d5c17149f01
TheRealSlimStefan <19smarcins98@gmail.com>
2021-11-30 18:55:17 +0100

    working page routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+    BrowserRouter as Router,
+    NavLink,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+const Home = () => <h1>Strona startowa</h1>;
+const News = () => <h1>Aktualności</h1>;
+const Contact = () => <h1>Kontakt</h1>;
+const ErrorPage = () => <h1>Strona nie istnieje</h1>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <header>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Start</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/news">Aktualności</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Kontakt</NavLink>
+                        </li>
+                    </ul>
+                </header>
+                <section>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/news" element={<News />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="*" element={<ErrorPage />} />
+                    </Routes>
+                </section>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
