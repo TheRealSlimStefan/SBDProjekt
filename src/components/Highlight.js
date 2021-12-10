a02fd1b08a07c1c558b4a43cf24abfe18c131352
TheRealSlimStefan <19smarcins98@gmail.com>
2021-12-10 14:29:02 +0100

    styled highlight component

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -19,7 +19,30 @@ const Highlight = () => {
                     <p>Jagiellonia Białystok - Legia Warszawa</p>
                 </div>
             </nav>
-            <div className="summary"></div>
+            <div className="summary">
+                <div className="date">
+                    <p>Koniec, 09.12.2021</p>
+                </div>
+                <div className="score">
+                    <div className="first">
+                        <img
+                            src="https://jagiellonia.pl/wp-content/uploads/2021/11/Herb_Jagiellonii.png"
+                            alt=""
+                        />
+                        <p>Jagiellonia Białystok</p>
+                    </div>
+                    <p>1</p>
+                    <p>-</p>
+                    <p>1</p>
+                    <div className="second">
+                        <img
+                            src="https://www.logofootball.net/wp-content/uploads/Legia-Warszawa-FC-Logo.png"
+                            alt=""
+                        />
+                        <p>Legia Warszawa</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
